b7182817113f69ba20d28999fe1b3e79763b50c2
P4R-D4RK <luis.282001@gmail.com>
2025-06-11 11:35:59 -0600

    new: Project created

---
+++ payment-system-backend/src/main/java/com/p4r/paymentsystem/PaymentSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.p4r.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemBackendApplication.class, args);
+	}
+
+}
---
+++ payment-system-backend/src/test/java/com/p4r/paymentsystem/PaymentSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p4r.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
